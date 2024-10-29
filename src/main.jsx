6c0379fd9c1632a7fd42c0f1bf1f1d43a65ad995
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-10-29 02:20:39 -0300

    inserindo crud nos appointments, finalizando aplicação e preparando para receber os dados da API

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import "./styles/global.css";
-import React from "react";
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 import Rotas from "./rotas.jsx";
 
